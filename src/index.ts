15fa86f3ef24ac96cdf2aaf0e00b3cd7506a64a5
Shubham Chaturvedi <19shubham11@gmail.com>
2021-03-15 14:49:09 +0100

    project setup

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+function main(name: string): string {
+    return `Hello ${name}`
+}
---
+++ tests/random.test.ts
@@ -0,0 +1,7 @@
+import assert from 'assert'
+
+describe('Test', () => {
+    it('Should work', () => {
+        assert.strictEqual(1, 1)
+    })
+})
