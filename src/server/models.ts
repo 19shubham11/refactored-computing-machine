8a00574f4fde1ab1fec4177bd7a52a1146bb5887
Shubham Chaturvedi <19shubham11@gmail.com>
2021-04-02 22:44:32 +0200

    replaced express and used fastify

---
+++ src/server/models.ts
@@ -0,0 +1,5 @@
+interface QueryParams {
+    q?: string
+}
+
+export { QueryParams }
--- tests/server/server.test.ts
+++ tests/server/server.test.ts
@@ -1,47 +1,48 @@
-import supertest from 'supertest'
-import express from 'express'
-import { router } from '../../src/server/routes'
+import fastify from 'fastify'
 import assert from 'assert'
+import { router } from '../../src/server/routes'
 
-const app = express()
-app.use('/', router)
-
-const request = supertest(app)
+const server = fastify()
+server.register(router)
 
 describe('API Integration Tests', () => {
+    afterAll(async () => {
+        await server.close()
+    })
+
     describe('/internal/health', () => {
         it('Should return 200', async () => {
-            const res = await request.get('/internal/health')
-            const { status, text } = res
+            const res = await server.inject().get('/internal/health')
+            const { statusCode, body } = res
 
-            assert.strictEqual(status, 200)
-            assert.strictEqual(text, 'OK')
+            assert.strictEqual(statusCode, 200)
+            assert.strictEqual(body, 'OK')
         })
     })
 
     describe('/random', () => {
         it('Should redirect', async () => {
-            const res = await request.get('/random')
-            const { status } = res
+            const res = await server.inject().get('/random')
+            const { statusCode } = res
 
-            assert.strictEqual(status, 302)
+            assert.strictEqual(statusCode, 302)
         })
     })
 
     describe('/random/search', () => {
         it('Should redirect', async () => {
-            const res = await request.get('/random/search?q=hello')
-            const { status } = res
+            const res = await server.inject().get('/random/search?q=dog')
+            const { statusCode } = res
 
-            assert.strictEqual(status, 302)
+            assert.strictEqual(statusCode, 302)
         })
 
         it('Should return 400 if query param was not present', async () => {
-            const res = await request.get('/random/search')
-            const { status, text } = res
+            const res = await server.inject().get('/random/search')
+            const { statusCode, body } = res
 
-            assert.strictEqual(status, 400)
-            assert.match(text, /Add search param/)
+            assert.strictEqual(statusCode, 400)
+            assert.match(body, /Add search param/)
         })
     })
 })
